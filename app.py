62d16b3d6e751dcc522f4688edea7705ba44e2f3
Pranathi-Shetty <shettypranathi77@gmail.com>
2026-02-17 19:13:18 +0530

    Updated CORS configuration

--- app.py
+++ app.py
@@ -2,7 +2,12 @@ from flask import Flask, request, jsonify
 from flask_cors import CORS
 
 app = Flask(__name__)
-CORS(app)
+CORS(app, resources={r"/*": {"origins": [
+    "http://localhost:5173",
+    "https://your-vercel-domain.vercel.app"
+]}})
+
+
 
 # Fake database (temporary)
 users = {
